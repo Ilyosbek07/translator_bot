f4b5b5447a1deae40b563944e2b37f6cec77ace0
Ilyosbek07 <qarshiboyevilyos18@gmail.com>
2023-02-01 20:34:47 +0500

    first committt

--- handlers/users/__init__.py
+++ handlers/users/__init__.py
@@ -2,3 +2,4 @@ from . import help
 from . import admin
 from . import admin2
 from . import start
+from . import echo
--- handlers/users/start.py
+++ handlers/users/start.py
@@ -92,20 +92,20 @@ async def checker(call: types.CallbackQuery, state: FSMContext):
                                      disable_web_page_preview=True)
 
 
-@dp.message_handler(state=None)
-async def bot_echo(message: types.Message):
-    try:
-        langs = await db.get_from(tg_id=message.from_user.id)
-        from_lang = ''
-        to_lang = ''
-        for i in langs:
-            from_lang += f"{i[0]}"
-            to_lang += f"{i[1]}"
-
-        translator = Translator(from_lang=f'{from_lang}', to_lang=f"{to_lang}")
-        text = message.text
-        a = translator.translate(f"{text}")
-        await message.answer(text=a, disable_notification=True)
-    except:
-        await message.answer('Kechirasiz faqat 500 tagacha bo`lgan so`zlarni qabul qilamiz\n\n'
-                             'Iltomos 500 tadan ortig`ini alohida kiriting')
+# @dp.message_handler(state=None)
+# async def bot_echo(message: types.Message):
+#     try:
+#         langs = await db.get_from(tg_id=message.from_user.id)
+#         from_lang = ''
+#         to_lang = ''
+#         for i in langs:
+#             from_lang += f"{i[0]}"
+#             to_lang += f"{i[1]}"
+#
+#         translator = Translator(from_lang=f'{from_lang}', to_lang=f"{to_lang}")
+#         text = message.text
+#         a = translator.translate(f"{text}")
+#         await message.answer(text=a, disable_notification=True)
+#     except:
+#         await message.answer('Kechirasiz faqat 500 tagacha bo`lgan so`zlarni qabul qilamiz\n\n'
+#                              'Iltomos 500 tadan ortig`ini alohida kiriting')
