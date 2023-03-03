895d3fad475a0ec3705ae19a1d77b50f534e1614
Ilyosbek <77951978+Ilyosbek07@users.noreply.github.com>
2023-03-03 21:10:18 +0500

    ru

--- handlers/users/start.py
+++ handlers/users/start.py
@@ -27,7 +27,7 @@ async def bot_start(message: types.Message):
 
 @dp.message_handler(commands='uzru')
 async def bot_start(message: types.Message):
-    a = await db.update_users_from_lang(from_lang='uz', to_lang='ar', tg_id=message.from_user.id)
+    a = await db.update_users_from_lang(from_lang='uz', to_lang='ru', tg_id=message.from_user.id)
     await message.answer(
         'Hozir "O`zbek - Rus" tarjima holatidasiz. "Rus - O`zbek" holatiga o`tish uchun /ruuz buyrug`ini bering.')
 
