9529aa61a487a33d3f49b69d324cae678136772d
Ilyosbek <77951978+Ilyosbek07@users.noreply.github.com>
2023-03-31 09:22:53 +0500

    new

--- utils/set_bot_commands.py
+++ utils/set_bot_commands.py
@@ -5,7 +5,7 @@ async def set_default_commands(dp):
     await dp.bot.set_my_commands(
         [
             types.BotCommand("start", "Botni ishga tushurish"),
-            types.BotCommand("ruuz", "Arab - O'zbek"),
+            types.BotCommand("arabuz", "Arab - O'zbek"),
             types.BotCommand("uzarab", "O'zbek - Arab"),
         ]
     )
